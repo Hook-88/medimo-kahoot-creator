91fa3b3b0dfe8a98cc360a5ec3eef69eda0e5ca5
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-06-02 10:02:39 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header class="text-3xl">
+    This is the header
+  </header>
+
+  <RouterView />
+</template>
